996a52a65a191e51f887a92a082c4d95fbbe7970
Tom Stahl <madflytom@gmail.com>
2022-11-24 05:26:27 -0500

    Initial Commit

---
+++ Magic 8 Ball/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Magic 8 Ball
+//
+//  Created by Tom Stahl on 11/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! First App")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Magic 8 Ball/Magic_8_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_BallApp.swift
+//  Magic 8 Ball
+//
+//  Created by Tom Stahl on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8_BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
